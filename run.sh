897d3592ed3809a7a575c32a8dde54bb3265c7b3
wanghezhang <wanghezhang@sensetime.com>
2019-03-13 10:41:46 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo $*
+GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:1 --ntasks-per-node=1 python -u main.py --arch=vgg11 --save-dir=./checkpoints | tee -a log
+# --load-path=checkpoint/res50/ckpt.pth.tar \
+# --resume-opt
+# --evaluate \
