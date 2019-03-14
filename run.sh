e6dcc23b2dd0e8199337eabc34b0642b2fdb41fb
wanghezhang <wanghezhang@sensetime.com>
2019-03-14 11:56:53 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo $*
-GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:1 --ntasks-per-node=1 python -u main.py --arch=vgg11 --save_dir=./checkpoints | tee -a log
+GLOG_vmodule=MemcachedClient=-1 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:1 --ntasks-per-node=1 python -u main.py --arch=vgg11 --save_dir=./checkpoints --epoches=100 | tee -a log
 # --load-path=checkpoint/res50/ckpt.pth.tar \
 # --resume-opt
 # --evaluate \
